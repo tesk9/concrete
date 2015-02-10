71d2fcb7a001d02dc1d228d1d778e6a07b58e283
Connor Kojimoto <cokojimoto@gmail.com>
2015-02-10 11:17:42 -0800

    add correct api key

--- config/env/locals.js
+++ config/env/locals.js
@@ -1,4 +1,4 @@
 process.env.USA_TODAY_API_KEY = "twkzdxs3fpf8vwdwacx53z3w";
-process.env.GOOGLE_MAPS_API = "AIzaSyD1xQ8m_sQA8m2wDDP6iroUlHf2ei2ehuc";
+process.env.GOOGLE_MAPS_API = "AIzaSyBN91gv43lq8daEUb1PKXGEWcmFCw2D5oo";
 
 module.exports = {};
