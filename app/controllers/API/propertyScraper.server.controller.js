ac57cd9156842f5ab74d4b37322178651dacfd8b
TaeHoon Yoon <thyoon220@gmail.com>
2015-02-10 15:00:45 -0800

    Deletes Cheerio.

--- app/controllers/API/propertyScraper.server.controller.js
+++ app/controllers/API/propertyScraper.server.controller.js
@@ -7,7 +7,6 @@ var mongoose = require('mongoose');
 var _ = require('lodash');
 var fs = require('fs');
 var request = require('request');
-var cheerio = require('cheerio');
 var properties = require('./seed_property.js')();
 
 module.exports = function(){
